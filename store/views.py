25b32d28ddfefc147c764d29235b486d6d25917e
Bonagiri sai santosh <bonagirisaisantosh2004@gmail.com>
2023-06-27 10:22:53 +0530

    first commit

---
+++ store/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import *
+def store(request):
+	producs=Product.objects.all()
+	context = {'products':producs}
+	return render(request, 'store/store.html', context)
+
+def cart(request):
+	context = {}
+	return render(request, 'store/cart.html', context)
+
+def checkout(request):
+	context = {}
+	return render(request, 'store/checkout.html', context)
